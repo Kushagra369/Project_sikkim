19ece86e715d26b403b971464793d32fb361a19e
Kushagra Saxena <64200696+Kushagra369@users.noreply.github.com>
2020-07-30 04:50:18 +0530

    Add files via upload

---
+++ databaseconnection/dbconfig.php
@@ -0,0 +1,6 @@
+<?php
+$link=mysqli_connect("localhost","root","","userlogs");
+if(mysqli_connect_error()){
+  die ("There Was An Error Connecting Database");
+}
+?>
---
+++ libraries/links.php
@@ -0,0 +1,5 @@
+<link href="..\vendor\twbs\bootstrap\dist\css\bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="..\vendor\twbs\bootstrap\dist\js\bootstrap.min.js"></script>
+  <script src="..\vendor\node_modules\jquery\dist\jquery.min.js"></script>
+  <link rel="stylesheet" href="..\vendor\fontawesome\css\all.css">
+  <link rel="stylesheet" href="css\fontastic.css">
